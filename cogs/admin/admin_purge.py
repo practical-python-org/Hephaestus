d8dc5e073b2b96df024be511117c96f327e87207
Adam Miranda-Artizada <Aartizada@gmail.com>
2023-04-14 13:43:33 +0200

    Fixed Purge

--- cogs/admin/admin_purge.py
+++ cogs/admin/admin_purge.py
@@ -14,20 +14,15 @@ class admin_purge(commands.Cog, command_attrs=dict(hidden=True)):
     async def purge_messages(self, ctx, number_messages: discord.Option(str)):
 
         # removes the need for a response
-        await ctx.defer()
         logs_channel = await self.bot.fetch_channel(config['mod_log'])  # Welcome channel
 
-        # Check that users are not removing from log channels
-        if ctx.channel.id in logs_channel.values():
-            await ctx.channel.send(f'{ctx.author.mention}, you should not be purging the {ctx.channel.mention}.')
-            await logs_channel.send(f'{ctx.author.mention} attempted to purge the {ctx.channel.mention}.')
-        else:
-            # Do the purge
-            await ctx.channel.purge(limit=int(number_messages))
-            # Log the purge
-            log_info(f"{ctx.author} purged {number_messages} messages from {ctx.channel}.")
-            await logs_channel.send(
-                f'{number_messages} messages purged from {ctx.channel.mention} by {ctx.author.mention}.')
+        # Do the purge
+        await ctx.channel.purge(limit=int(number_messages))
+        await ctx.respond(f"{ctx.author} removed {number_messages} messages.")
+        # Log the purge
+        log_info(f"{ctx.author} purged {number_messages} messages from {ctx.channel}.")
+        await logs_channel.send(
+            f'{number_messages} messages purged from {ctx.channel.mention} by {ctx.author.mention}.')
 
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
         """
