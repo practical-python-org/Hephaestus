0afe06394dcbf217efdc650df24b94b9bccaf176
James Massey <33434582+JamMassey@users.noreply.github.com>
2023-04-26 01:14:25 +0100

    testing (#3)
    * testing
    
    * fixes
    
    * fixes to manifest
    
    * fixes to manifest
    
    * dont have permissions to allow commented test results, so rolled back to automated testing

--- setup.py
+++ setup.py
@@ -5,23 +5,21 @@ setuptools.setup(
     description="An installable version of Hephaestus, primarily for testing purposes.",
     package_dir={"": "./"},
     packages=setuptools.find_packages(where="./"),
-        package_data={
-        'hephaestus': ['*.toml']
-    },
+    package_data={"hephaestus": ["*.toml"]},
     include_package_data=True,
     install_requires=[
-        "aiohttp==3.8.4"
-        "aiosignal==1.3.1"
-        "async-timeout==4.0.2"
-        "attrs==22.2.0"
-        "charset-normalizer==3.1.0"
-        "py-cord==2.4.1"
-        "frozenlist==1.3.3"
-        "idna==3.4"
-        "multidict==6.0.4"
-        "toml==0.10.2"
-        "yarl==1.8.2"
-        "pandas==2.0.0"
+        "aiohttp==3.8.4",
+        "aiosignal==1.3.1",
+        "async-timeout==4.0.2",
+        "attrs==22.2.0",
+        "charset-normalizer==3.1.0",
+        "py-cord==2.4.1",
+        "frozenlist==1.3.3",
+        "idna==3.4",
+        "multidict==6.0.4",
+        "toml==0.10.2",
+        "yarl==1.8.2",
+        "pandas==2.0.0",
     ],
     classifiers=[
         # see https://pypi.org/classifiers/
