29a61298ff3e251c795a29cbfd4a3ccf1b9eb69e
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-04-27 12:48:08 +0200

    linting AdminEmergency

--- bot/cogs/commands/admin_emergency.py
+++ bot/cogs/commands/admin_emergency.py
@@ -1,8 +1,18 @@
+"""
+This is a Staff only command
+It works by removing all permissions from every user except those with Admin permissions.
+/unlock is a reversal of the lockdown.
+"""
 from discord.ext import commands
-from logs.logger import log_info
+from bot.logs.logger import log_info
 
 
-class admin_emergency(commands.Cog):
+class AdminEmergency(commands.Cog):
+    """
+    Split into:
+        lockdown
+        unlock
+    """
     def __init__(self, bot):
         self.bot = bot
 
@@ -11,6 +21,9 @@ class admin_emergency(commands.Cog):
     )
     @commands.has_role('Staff')
     async def lockdown(self, ctx):
+        """
+        Sets all channel permissions to send_messages=False
+        """
         if not ctx.author.guild_permissions.administrator:
             return ctx.channel.send("You dont have permission to run a lockdown.")
         log_info("Initiated a lockdown.")
@@ -22,6 +35,9 @@ class admin_emergency(commands.Cog):
     @commands.slash_command(description="Reinstates all permissions to everyone in the server.")
     @commands.has_role('Staff')
     async def unlock(self, ctx):
+        """
+        re-instates all channel permissions to send_messages=None
+        """
         if not ctx.author.guild_permissions.administrator:
             return ctx.channel.send("You dont have permission to lift a lockdown.")
         log_info("Releasing lockdown.")
@@ -31,16 +47,24 @@ class admin_emergency(commands.Cog):
                 await channel.send(channel.mention + " ***has been unlocked.***")
 
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        """
+        Mostly permissions error handling.
+        """
         if isinstance(error, commands.MissingPermissions):
             await ctx.channel.send(
-                f"Sorry, {ctx.author.name}, you dont have the correct permissions to use this command!",
+                f"Sorry, {ctx.author.name}, you dont have the"
+                "correct permissions to use this command!",
                 reference=ctx.message)
         elif isinstance(error, commands.MissingRole):
-            await ctx.channel.send(f"Sorry, {ctx.author.name}, you must be a member of Staff to use this command!",
+            await ctx.channel.send(f"Sorry, {ctx.author.name}, "
+                                    "you must be a member of Staff to use this command!",
                                    reference=ctx.message)
         else:
             raise error
 
 
 def setup(bot):
-    bot.add_cog(admin_emergency(bot))
+    """
+    Necessary for loading the cog into the bot instance.
+    """
+    bot.add_cog(AdminEmergency(bot))
